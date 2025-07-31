789b71f00e3c21aa3be9bfe3fe4a8d473dc40032
Agustin <brian.barrionuevo@mi.unc.edu.ar>
2025-07-31 00:43:23 -0300

    Fix some bugs and setting main file

--- face_auth/compare.py
+++ face_auth/compare.py
@@ -3,26 +3,30 @@ import json
 import face_recognition
 import numpy as np
 
-"""En este archivo vamos a comparar los vectores embebidos 
+"""
+En este archivo vamos a comparar los vectores embebidos 
 segun la distancia entre ellos para detectar rostros  cuando
-dos rostros son distintos o son el mismo"""
+dos rostros son distintos o son el mismo
+"""
 def compare_faces(id_user):
     try:
         directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ids")
         id_json = id_user + ".json"
         user_path = os.path.join(directory, id_json)
-
+        """
+        Creamos el vector embebido del usuario ingresado
+        """
         if not os.path.exists(user_path):
             print("No se encontró la información del usuario en la base de datos")
             return
-
-        # Cargar embedding del usuario
         with open(user_path, "r") as f:
             data_user = json.load(f)
         vector_user = np.array(data_user[0]["embedding"])
 
         json_list = get_info_json()
-        # Comparar contra todos los embeddings en la carpeta
+        """
+        Comparamos el vector del usuario con los ya registrados 
+        """
         found_match = False
         for j in json_list:
             if j == id_json:
@@ -34,9 +38,8 @@ def compare_faces(id_user):
             
             vector_db = np.array(data_db[0]["embedding"])
 
-            # Comparar
             result = face_recognition.compare_faces([vector_db], vector_user)
-            print(f"Comparando con {j} -> {result[0]}")
+            #print(f"Comparando con {j} -> {result[0]}")
             
             if result[0]:
                 found_match = True
@@ -59,4 +62,5 @@ def get_info_json():
         if f.endswith(".json"):
             names_of_files.append(f)
     return names_of_files
+
 
--- face_auth/main.py
+++ face_auth/main.py
@@ -7,8 +7,15 @@ print("******************************Bienvenido******************************\n"
 response_user = input("0 == Start sesion || 1 == Register :")
 if int(response_user) == 0:
     print("Vamos a utilizar su camara para obtener su id\n")
+    print("Al momento de guardar la imagen, guardarla con su nombre de usuario")
     get_face.get_face()
-    id_user = input("Usuario:")
+    id_user = input("Introduci tu Usuario:")
     face_recog.get_vector(id_user)
     compare.compare_faces(id_user)
+elif int(response_user) == 1:
+    print("Vamos a utilizar su camara para añadir su cara a la base de datos\n")
+    print("Al momento de guardar la imagen, guardarla con su nombre de usuario")
+    get_face.get_face()
+    id_user = input("Introduci un nombre de usuario: ")
+    face_recog.get_vector(id_user)
     
