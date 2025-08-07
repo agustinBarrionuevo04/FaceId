f6791a5c3692d8b7c653ae78b72b448c067eff0e
Agustin <brian.barrionuevo@mi.unc.edu.ar>
2025-08-07 16:23:19 -0300

    fix conection to data base

--- face_auth/conn_db.py
+++ face_auth/conn_db.py
@@ -1,13 +1,7 @@
-# db_manager.py (versión TEXT)
 import psycopg
 import os
 import json
 import numpy as np
-# import face_recog
-# import get_face
-# import face_recognition
-
-DATA_BASE_URL = os.getenv("DATABASE_URL")
 
 
 def np_array_to_Json(vector):
@@ -17,20 +11,25 @@ def np_array_to_Json(vector):
 def json_to_np_array(vector_string):
     return np.array(json.loads(vector_string))
 
-""" Agrega un usuario a la base de datos"""
+
+def get_db_connection():
+    db_url = os.getenv("DATA_BASE")
+    if not db_url:
+        raise ValueError("La variable de entorno DATA_BASE no está definida.")
+    return psycopg.connect(db_url, sslmode='require')
+
 
 def new_user(name, vector_json):
-    conn = psycopg.connect(DATA_BASE_URL)
+    conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO "usuarios" (name,vector) VALUES (%s,%s)',(name, vector_json))
+    cursor.execute('INSERT INTO "usuarios" (name, vector) VALUES (%s, %s)', (name, vector_json))
     conn.commit()
     conn.close()
 
 
 def exist_user(vector_json):
-    conn = psycopg.connect(DATA_BASE_URL)
+    conn = get_db_connection()
     cursor = conn.cursor()
-
     vector = json_to_np_array(vector_json)
     cursor.execute('SELECT * FROM usuarios')
     row = cursor.fetchone()
@@ -38,12 +37,13 @@ def exist_user(vector_json):
         vector_db = json_to_np_array(row[2])
         exist_face = compare_vectors(vector, vector_db)
         if exist_face:
+            conn.close()
             return exist_face, row[1]
         row = cursor.fetchone()
     conn.close()
     return False, None
 
 
-def compare_vectors(vec1, vec2, threshold=0.6): 
+def compare_vectors(vec1, vec2, threshold=0.6):
     dist = np.linalg.norm(vec1 - vec2)
     return dist < threshold
